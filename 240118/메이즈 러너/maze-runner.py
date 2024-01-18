4ad27205b62d1c8e615eddbe8c517c7e857109d1
ahnha-ahnha <89643634+ahnha-ahnha@users.noreply.github.com>
2024-01-18 17:14:47 +0900

    Title: 메이즈 러너, Time: 217ms, Memory: 29MB, Status: Passed - CodeTree

--- 240118/메이즈 러너/maze-runner.py
+++ 240118/메이즈 러너/maze-runner.py
@@ -1,123 +1,112 @@
 import sys
-from collections import deque
+import copy
 
 input = sys.stdin.readline
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+#input----------
+# def -1
+def int_minus(x):
+    return int(x)-1
+n, m , k  = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)] # 벽 위치
+people = [tuple(map(int_minus, input().split())) for _ in range(m)] # 사람위치 좌표 (+1씩 되어있음)
 
-n, m, k = map(int, input().split())
+# 출력해야할 정답값
+pathn = 0 # 모든 참가자들의 이동 거리 합
+exit = tuple(map(int_minus, input().split()))
 
-# q에 참가자의 좌표를 저장
-a = [list(map(int, input().split())) for _ in range(n)]
-q = deque()
-for i in range(m):
-    x, y = map(int, input().split())
-    q.append([x - 1, y - 1])
 
-# 출구의 좌표를 저장하고 지도에 값을 10으로 한다
-ex, ey = map(int, input().split())
-ex -= 1
-ey -= 1
-a[ex][ey] = 10
+# move ============
+# 1) 움직일 수 있는 곳 확인 -> eixt과 값 빼기
+# 2) 움직이기 - pathn 계산
+def move(pathn):
+    global people
+    for i, p in enumerate(people):
+        row = exit[0]- p[0] # row방 어디로 얼마나 가야하는지
+        column = exit[1]-p[1] # column방향 어디로 얼마나 가야하는지
+        # 움직이는 우선순위: row -> column / 그 방향이 벽이 아니여야 가능
+        # row먼저 check
+        if row !=0:
+            move = 1 if row>0 else -1
+            newpr, newpc = p[0]+move, p[1]
+            if 0<=newpr<n and 0<=newpc<n and a[newpr][newpc]==0:
+                #움직일 수 있음
+                people[i] = (newpr, newpc)
+                pathn +=1
+                continue # 움직였으면, 감
+        # 그다음 column check -> elif 대신 if써야함
+        if column !=0:
+            move = 1 if column>0 else -1
+            newpr, newpc = p[0], p[1] + move
+            if 0<=newpr<n and 0<=newpc<n and a[newpr][newpc]==0:
+                #움직일 수 있음
+                people[i] = (newpr, newpc)
+                pathn +=1
+    return pathn
 
-ans = 0
-for _ in range(k):
-    qlen = len(q)
-    # 현재 q에 있는 좌표만 불러와서 이동한다
-    for _ in range(qlen):
-        try:
-            x, y = q.popleft()
-            length = abs(ex - x) + abs(ey - y)
-            cand = []
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < n:
-                    if a[nx][ny] == 0:
-                        # 이동했을때 출구와의 거리가 더 가까워지면 cand에 저장
-                        n_length = abs(ex - nx) + abs(ey - ny)
-                        if n_length < length:
-                            cand.append([nx, ny])
-                    elif a[nx][ny] == 10:
-                        # 출구에 도착하면 즉시 종료한다
-                        raise NotImplemented
+# distance (rotate box search) ============
+def distance(one, two):
+    # rotation 을 위한 distance계산
+    # input: 두 좌표 (하나는 출구, 하나는 사람)
+    # ouput: (distance, 작은 r좌표, 작은 c좌표)
+    row = abs(one[0]-two[0])
+    column = abs(one[1] - two [1])
+    d,r,c = max(row, column), min(one[0], two[0]), min(one[1], two[1])
+    if row>column:
+        # 변의 길이를 결정하는게 row라면, row는 그대로. column시작점을 옮길 수 있음
+        c = max(one[1], two[1])- d
+        while c<0:
+            c+=1
+        return (d,r,c)
+    elif row<column:
+        r = max(one[0], two[0])- d
+        while r<0:
+            r+=1
+        return (d,r,c)
+    else:
+        return (d,r,c)
 
-            if cand:
-                # bfs에서 상하 이동을 먼저하기 때문에 cand[0]이 다음 좌표가 된다
-                nx, ny = cand[0]
-                ans += 1
-                q.append([nx, ny])
-            else:
-                # 이동이 불가능하면 처음좌표 [x, y]를 다시 넣어준다
-                q.append([x, y])
+# rotate ============
+def rotate():
+    global a, people, exit
+    # 1)  rotate할 위치 찾기-------
+    distancelist =[]
+    for p in people:
+        distancelist.append(distance(exit, p))
+    distancelist= sorted(distancelist) # distance, r, c 순으로 작은것 순으로 배열
+    d, r, c = distancelist[0] # roated할 크기, distance 정해짐
 
-        except:
-            ans += 1
-    
-    # 참가자가 없으면 종료
-    if len(q) == 0:
+    #2) rotate하기 --------
+    tempa=copy.deepcopy(a) #
+    newpeople =[]
+    newexit =exit
+    for i in range(d+1):
+        for j in range(d+1):
+            if tempa[r+i][c+j] >0: # 2) 벽 -1
+                tempa[r+i][c+j]-=1
+            a[r+j][c+d-i] = tempa[r+i][c+j] # 1)공간 rotate
+            while (r+i, c+j) in people: # 3) people 안에 있으면, 사람도 rotate된 값으로
+                people.remove((r+i, c+j))
+                newpeople.append((r+j, c+d-i))
+            if (r+i,c+j) == exit: # 4) exit rotate & update
+                newexit =(r+j, c+d-i)
+
+    people = people+newpeople # 2) people update
+    exit = newexit
+
+
+# main ============
+for i in range(k):
+    pathn = move(pathn) # 1) 움직이기
+
+    while exit in people: # 2) exit으로 나간 것 정리
+        people.remove((exit))
+    if len(people) ==0:
         break
-    
-    # 브루트포스로 참가자와 출구를 포함한 최소 정사각형을 찾는다
-    try:
-        for l in range(1, 10):
-            for i in range(n):
-                for j in range(n):
-                    if j + l >= n:
-                        break
-                    if i <= ex <= i + l and j <= ey <= j + l:
-                        for x, y in q:
-                            if i <= x <= i + l and j <= y <= j + l:
-                                tx, ty, length = i, j, l
-                                raise NotImplemented
-    except:
-        pass
-    
-    # 참가자 위치를 회전시키기 위해서 참가자 좌표 기록을 위한 c를 만든다
-    # 참가자가 있는 좌표는 c에서 해당좌표 값에 1을 더해준다
-    c = [[0 for _ in range(n)] for _ in range(n)]
-    while q:
-        x, y = q.popleft()
-        c[x][y] += 1
-    
-    # rotate에 정사각형 범위만큼의 c를 넣어준다
-    rotate = []
-    for i in range(length + 1):
-        rotate.append(c[tx + i][ty:ty + length + 1])
-    # rotate를 회전시킨다
-    rotate = list(zip(*rotate[::-1]))
-    for i in range(length + 1):
-        rotate[i] = list(rotate[i])
-    # 정사각형 범위만큼 c와 rotate를 교체한다
-    for i in range(length + 1):
-        c[tx + i][ty:ty + length + 1] = rotate[i]
-    
-    # c에서 0이 아닌 좌표를 해당 값만큼 q에 넣어준다
-    for i in range(n):
-        for j in range(n):
-            if c[i][j] > 0:
-                for _ in range(c[i][j]):
-                    q.append([i, j])
+    rotate() #3) 돌리기
 
-    # 위에서 참가자 좌표를 회전시키는 것과 똑같이 지도 a를 회전시킨다
-    rotate = []
-    for i in range(length + 1):
-        rotate.append(a[tx + i][ty:ty + length + 1])
-    rotate = list(zip(*rotate[::-1]))
-    for i in range(length + 1):
-        rotate[i] = list(rotate[i])
-        # 벽은 1을 빼준다
-        for j in range(length + 1):
-            if 0 < rotate[i][j] < 10:
-                rotate[i][j] -= 1
-    for i in range(length + 1):
-        a[tx + i][ty:ty + length + 1] = rotate[i]
-    
-    # 출구 좌표를 수정해준다
-    for i in range(n):
-        for j in range(n):
-            if a[i][j] == 10:
-                ex, ey = i, j
 
-print(ans)
-print(ex + 1, ey + 1)
+
+#정답 출력
+print(pathn)
+print(exit[0]+1, end=" ")
+print(exit[1]+1)
