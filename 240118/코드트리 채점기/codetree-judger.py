40ea2091006bffb847db0525d5d67675d8f87d66
ahnha-ahnha <89643634+ahnha-ahnha@users.noreply.github.com>
2024-01-18 17:38:39 +0900

    Title: 코드트리 채점기, Time: 1214ms, Memory: 39MB, Status: Passed - CodeTree

--- 240118/코드트리 채점기/codetree-judger.py
+++ 240118/코드트리 채점기/codetree-judger.py
@@ -1,42 +1,76 @@
-from heapq import heappush, heappop
-import heapq
+from heapq import heappush, heappop, heapify
+import sys
+input = sys.stdin.readline
+MAX = float('inf')
 
 Q = int(input())
-command = list(input().split())
-J = int(command[1])  # 채점기 개수
-Waiting_Judger = list(range(1, J + 1))
-Waiting_Queue = []  # 채점 대기 큐 (우선순위, 시간, URL)
-Judging = {}  # 현재 채점기 작동 J_id : [시작시간, URL]
-History = {}  # 도메인별 마지막 채점 시간 기록 Domain : [Start, Gap]
+querys = [input().split() for _ in range(Q)]
+grading_domain = dict()
+prev_ended_domain = dict()
+domains = set()
+waited_task = dict()
+waited_url = set()
+tot_waited = 0
 
-heappush(Waiting_Queue, (0, 1, command[2]))
+for i in range(Q) :
+    query, *body = querys[i]
+    if query <= '200' :
+        domain = body[-1].split('/')[0]
+        domains.add(domain)
+        grading_domain[domain] = False
+        prev_ended_domain[domain] = -1
+        if domain not in waited_task :
+            waited_task[domain] = list()
 
-for _ in range(Q - 1):
-    command = list(input().split())
-    cmd_type, t = int(command[0]), int(command[1])
+_, N, u0 = querys[0]
+d0 = u0.split('/')[0]
+graders = [(-1,-1) for _ in range(int(N))]
+empty_graders = list(range(int(N)))
+heapify(empty_graders)
+heappush(waited_task[d0], (1, 0, u0))
+waited_url.add(u0)
+tot_waited += 1
 
-    if cmd_type == 200:
-        p, u = int(command[2]), command[3]
-        # 채점 대기 큐에서 같은 URL이 있는지 확인
-        if not any(u == queued_u for _, _, queued_u in Waiting_Queue):
-            heappush(Waiting_Queue, (p, t, u))
+for i in range(1, Q) :
+    query, *body = querys[i]
+    if query == '200' :
+        t, p, u = body
+        d = u.split('/')[0]
+        if u in waited_url :
+            continue
+        waited_url.add(u)
+        heappush(waited_task[d], (int(p), int(t), u))
+        tot_waited += 1
+    elif query == '300' :
+        if not empty_graders or not tot_waited:
+            continue
+        t = int(body[0])
+        target_d, priority, target_u = '', (MAX, MAX), ''
+        for d in domains :
+            if grading_domain[d] or prev_ended_domain[d] > t or not waited_task[d]:
+                continue
+            tp, tt, tu = waited_task[d][0]
+            if priority <= (tp, tt) :
+                continue
+            target_d, priority, target_u = d, (tp, tt), tu
 
-    elif cmd_type == 300:
-        if Waiting_Queue and Waiting_Judger:
-            while Waiting_Queue:
-                p, enter_time, url = heappop(Waiting_Queue)
-                if url not in History or t >= History[url][0] + 3 * History[url][1]:
-                    J_id = min(Waiting_Judger)
-                    Judging[J_id] = [t, url]
-                    Waiting_Judger.remove(J_id)
-                    break
+        if priority < (MAX, MAX) :
+            idx = heappop(empty_graders)
+            grading_domain[target_d] = True
+            waited_url.discard(target_u)
+            graders[idx] = (t, target_d)
+            heappop(waited_task[target_d])
+            tot_waited -= 1
 
-    elif cmd_type == 400:
-        J_id = int(command[2])
-        if J_id in Judging:
-            start_time, url = Judging.pop(J_id)
-            History[url] = [start_time, t - start_time]
-            Waiting_Judger.append(J_id)
-
-    elif cmd_type == 500:
-        print(len(Waiting_Queue))
+    elif query == '400' :
+        t, idx = map(int, body)
+        idx -= 1
+        if graders[idx] == (-1, -1) :
+            continue
+        s, d = graders[idx]
+        graders[idx] = (-1, -1)
+        grading_domain[d] = False
+        prev_ended_domain[d] = s + (t - s)*3
+        heappush(empty_graders, idx)
+    else :
+        print(tot_waited)
